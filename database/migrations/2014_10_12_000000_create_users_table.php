e1e0e97a2cfbaa0d998f77cf034cd2883c94e5c1
Sleepy4k <sarahpalastrin@gmail.com>
2022-11-25 20:30:02 +0700

    feat: user miragtion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
+            $table->string('is_admin')->default('false');
             $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
